6cddd462f0836748da17dc5f7374a7dd3328c52c
Michael Nett <mnett@google.com>
2014-12-27 19:27:24 +0900

    Renaming module containing main() and moving tensor helper functions out of anonymous namespace.

--- src/dense_tensor.h
+++ src/dense_tensor.h
@@ -11,38 +11,6 @@
 #include <type_traits>
 #include <vector>
 
-namespace {
-
-// Returns the product of two unsigned values. If the multiplication would create an overflow the
-// function dies instead.
-template <typename T>
-typename std::enable_if<std::is_arithmetic<T>::value&& std::is_unsigned<T>::value, T>::type
-MultiplyOrDie(const T lhs, const T rhs) {
-  if (lhs != T(0)) {
-      CHECK_GT(std::numeric_limits<T>::max() / lhs, rhs);
-  }
-  return lhs * rhs;
-}
-
-// TODO(mnett): Modify the order to maximize throughput for matrix operations [P=2].
-template <std::size_t P, std::size_t N>
-struct IndexHelper {
-  constexpr static inline std::uint64_t LinearizeIndex(const std::array<std::uint64_t, P>& index,
-                                                       const std::array<std::uint64_t, P>& size) {
-    return index[P - N - 1] + size[P - N - 1] * IndexHelper<P, N - 1>::LinearizeIndex(index, size);
-  }
-};
-
-template <std::size_t P>
-struct IndexHelper<P, 0> {
-  constexpr static inline std::uint64_t LinearizeIndex(const std::array<std::uint64_t, P>& index,
-                                                       const std::array<std::uint64_t, P>& size) {
-    return index[P - 1];
-  }
-};
-
-}  // namespace
-
 template <typename T, std::size_t P>
 class DenseTensor {
  public:
@@ -76,6 +44,38 @@ class DenseTensor {
   std::vector<T> data_;
 };
 
+namespace internal {
+
+// Returns the product of two unsigned values. If the multiplication would create an overflow the
+// function dies instead.
+template <typename T>
+typename std::enable_if<std::is_arithmetic<T>::value&& std::is_unsigned<T>::value, T>::type
+MultiplyOrDie(const T lhs, const T rhs) {
+  if (lhs != T(0)) {
+    CHECK_GT(std::numeric_limits<T>::max() / lhs, rhs);
+  }
+  return lhs * rhs;
+}
+
+// TODO(mnett): Modify the order to maximize throughput for matrix operations [P=2].
+template <std::size_t P, std::size_t N>
+struct IndexHelper {
+  constexpr static inline std::uint64_t LinearizeIndex(const std::array<std::uint64_t, P>& index,
+                                                       const std::array<std::uint64_t, P>& size) {
+    return index[P - N - 1] + size[P - N - 1] * IndexHelper<P, N - 1>::LinearizeIndex(index, size);
+  }
+};
+
+template <std::size_t P>
+struct IndexHelper<P, 0> {
+  constexpr static inline std::uint64_t LinearizeIndex(const std::array<std::uint64_t, P>& index,
+                                                       const std::array<std::uint64_t, P>& size) {
+    return index[P - 1];
+  }
+};
+
+}  // namespace internal
+
 template <typename T, std::size_t P>
 DenseTensor<T, P>::DenseTensor(const Index& size)
     : size_(size) {
@@ -85,20 +85,21 @@ DenseTensor<T, P>::DenseTensor(const Index& size)
 
 template <typename T, std::size_t P>
 const T& DenseTensor<T, P>::Get(const Index& index) const {
-  const auto linear_index = IndexHelper<P, P - 1>::LinearizeIndex(index, size_);
+  const auto linear_index = internal::IndexHelper<P, P - 1>::LinearizeIndex(index, size_);
   return data_[linear_index];
 }
 
 template <typename T, std::size_t P>
 DenseTensor<T, P>& DenseTensor<T, P>::Set(const Index& index, const T value) {
-  const auto linear_index = IndexHelper<P, P - 1>::LinearizeIndex(index, size_);
+  const auto linear_index = internal::IndexHelper<P, P - 1>::LinearizeIndex(index, size_);
   data_[linear_index] = value;
   return *this;
 }
 
 template <typename T, std::size_t P>
 void DenseTensor<T, P>::Allocate() {
-  const auto total_size = std::accumulate(size_.cbegin(), size_.cend(), 1, MultiplyOrDie<std::uint64_t>);
+  const auto total_size =
+      std::accumulate(size_.cbegin(), size_.cend(), 1, internal::MultiplyOrDie<std::uint64_t>);
   data_.resize(total_size);
 }
 
---
+++ src/playground.cc
@@ -0,0 +1,43 @@
+// CP-ALS of order p; overview:
+//
+// (1) Initialize each A^(n).
+// (2) Repeat until convergence
+//     (2.1) For each i = 1..p
+//           (2.1.1) Let C^(i) = adj(A^(i)) * A^(i)
+//           (2.1.2) Let C = CoeffProd_{j=1..p, j != i} C^(j)
+//           (2.1.3) Let A^(i) = X^(i) [KathriRao_{j=1..p, j != i} A^(j)] adj(C)
+//
+//
+// Required structures:
+//
+// (1) Sparse Tensor
+// (2) Dense Tensor
+//
+//
+// Required operations:
+//
+// (1)
+
+#include <glog/logging.h>
+
+#include <cstdio>
+
+#include "dense_tensor.h"
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+
+  DenseTensor<float, 3> X({{2, 2, 2}});
+
+  X.Set({{0, 0, 0}}, 1.0f);
+
+  for (std::uint64_t i = 0; i < 2; ++i) {
+    for (std::uint64_t j = 0; j < 2; ++j) {
+      for (std::uint64_t k = 0; k < 2; ++k) {
+        printf("%lu %lu %lu => %f\n", i, j, k, X.Get({{i, j, k}}));
+      }
+    }
+  }
+
+  return 0;
+}
