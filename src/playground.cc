d7f6baa9085651206144769dc1d69b6c86163efc
Michael Nett <mnett@google.com>
2015-01-03 20:54:59 +0900

    Removing playground code that is covered by sanity checks in test cases.

--- src/playground.cc
+++ src/playground.cc
@@ -22,22 +22,23 @@
 
 #include <cstdio>
 
-#include "dense_tensor.h"
+#include "tensor/dense_tensor.h"
+// #include "expr/scalar_product_expr.h"
 
 int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
 
   DenseTensor<float, 3> X({{2, 2, 2}});
 
-  X.Set({{0, 0, 0}}, 1.0f);
+  // X.Set({{0, 0, 0}}, 1.0f);
 
-  for (std::uint64_t i = 0; i < 2; ++i) {
-    for (std::uint64_t j = 0; j < 2; ++j) {
-      for (std::uint64_t k = 0; k < 2; ++k) {
-        printf("%lu %lu %lu => %f\n", i, j, k, X.Get({{i, j, k}}));
-      }
-    }
-  }
+  // for (std::uint64_t i = 0; i < 2; ++i) {
+  //   for (std::uint64_t j = 0; j < 2; ++j) {
+  //     for (std::uint64_t k = 0; k < 2; ++k) {
+  //       printf("%lu %lu %lu => %f\n", i, j, k, X.Get({{i, j, k}}));
+  //     }
+  //   }
+  // }
 
   return 0;
 }
