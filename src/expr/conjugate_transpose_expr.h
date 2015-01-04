2dccd9f630d751010b4ac2d39e7bba3e79af55b6
Simon Wollwage <mail.wollwage@gmail.com>
2015-01-04 12:33:50 +0100

    added conjugate transpose

--- src/expr/conjugate_transpose_expr.h
+++ src/expr/conjugate_transpose_expr.h
@@ -1,22 +1,23 @@
-#ifndef EXPR_TRANSPOSE_EXPR_H_
-#define EXPR_TRANSPOSE_EXPR_H_
+#ifndef EXPR_CONJUGATE_TRANSPOSE_EXPR_H_
+#define EXPR_CONJUGATE_TRANSPOSE_EXPR_H_
 
+#include <complex>
 #include "expr/matrix_expr.h"
 #include "tensor/storage_mode.h"
 
 namespace expr {
 
 template <typename ExprType, typename ValueType>
-class TransposeExpr
-    : public MatrixExpr<TransposeExpr<ExprType, ValueType>, ValueType> {
+class ConjugateTransposeExpr
+    : public MatrixExpr<ConjugateTransposeExpr<ExprType, ValueType>, ValueType> {
  public:
-  using base_type = MatrixExpr<TransposeExpr<ExprType, ValueType>, ValueType>;
+  using base_type = MatrixExpr<ConjugateTransposeExpr<ExprType, ValueType>, ValueType>;
   using size_type = typename base_type::size_type;
   using pos_type = typename base_type::pos_type;
   using value_type = typename base_type::value_type;
 
   // Create a scalar product expression representing the product of a scalar and a tensor.
-  TransposeExpr(const ExprType& expr);
+  ConjugateTransposeExpr(const ExprType& expr);
 
   pos_type dimensions() const override {
     return expr_.dimensions();
@@ -27,7 +28,7 @@ class TransposeExpr
   }
 
   const value_type Get(const pos_type& index) const override {
-    return expr_.Get(pos_type { index[1], index[0] });
+    return std::conj(expr_.Get(pos_type { index[1], index[0] }));
   }
 
   value_type abs() const override {
---
+++ src/test/conjugate_transpose_expr_test.cc
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+#include "expr/conjugate_transpose_expr.h"
+#include "tensor/storage_mode.h"
+#include "tensor/tensor.h"
+
+using tensor::StorageMode;
+using tensor::Tensor;
+
+namespace test {
+namespace {
+
+Tensor<float, 2> CreateTensor() {
+  Tensor<float, 2> tensor({{2, 2}}, StorageMode::DENSE);
+  tensor.Set({{0, 0}}, 1.0f);
+  tensor.Set({{0, 1}}, 2.0f);
+  tensor.Set({{1, 0}}, 3.0f);
+  tensor.Set({{1, 1}}, 4.0f);
+  return tensor;
+}
+
+}  // namespace
+
+TEST(ConjugateTransposeExprTest, SanityCheck) {
+  // Create a tensor and verify its norm.
+  // Tensor<float, 2> tensor = CreateTensor();
+  // EXPECT_FLOAT_EQ(5.477225575, tensor.abs());
+
+  // // Scaling a tensor by 1 or -1 must preserve its norm.
+  // {
+  //   auto pos_scaled = 1.0f * tensor;
+  //   EXPECT_FLOAT_EQ(5.477225575, pos_scaled.abs());
+  //   auto neg_scaled = -1.0f * tensor;
+  //   EXPECT_FLOAT_EQ(5.477225575, neg_scaled.abs());
+  // }
+
+  // // Tensor scaling should be positively homogenous.
+  // {
+  //   auto pos_scaled = 2.0f * tensor;
+  //   EXPECT_FLOAT_EQ(2.0f * 5.477225575f, pos_scaled.abs());
+  //   auto neg_scaled = -2.0f * tensor;
+  //   EXPECT_FLOAT_EQ(2.0f * 5.477225575f, neg_scaled.abs());
+  // }
+}
+
+}  // namespace test
