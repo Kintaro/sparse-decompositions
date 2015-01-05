9bf6fba98d98a06159d65b5ff4e24a49d242f6c8
Simon Wollwage <mail.wollwage@gmail.com>
2015-01-05 22:14:22 +0100

    added ! operator

--- src/expr/transpose_expr.h
+++ src/expr/transpose_expr.h
@@ -10,4 +10,10 @@ using TransposeExpr = ConjugateTransposeExpr<ExprType, ValueType, internal::Conj
 
 } // namespace expr
 
+template <typename ExprType, typename ValueType>
+const expr::TransposeExpr<ExprType, ValueType> operator!(
+            const expr::MatrixExpr<ExprType, ValueType>& expr) {
+      return expr::TransposeExpr<ExprType, ValueType>(expr);
+}
+
 #endif  // EXPR_TRANSPOSE_EXPR_H_
