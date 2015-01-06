a6c2e0db1bc4776a81c01ddc1f1a16c72d64c80d
Michael Nett <mnett@google.com>
2015-01-06 15:32:53 +0900

    Merged files for expression utils.

--- src/expr/expr_util.h
+++ src/expr/expr_util.h
@@ -7,11 +7,23 @@ namespace util {
 // Returns whether the content of two containers is identical. Containers of different size are
 // deemed non-identical.
 template <typename ContainerType>
-bool container_equals(const ContainerType& a, const ContainerType& b);
+bool container_equals(const ContainerType& a, const ContainerType& b) {
+  auto iter_a = a.cbegin();
+  auto iter_b = b.cbegin();
+  while ((iter_a != a.cend()) && (iter_b != b.cend())) {
+    if (*iter_a != *iter_b) {
+      return false;
+    }
+    ++iter_a;
+    ++iter_b;
+  }
+  if ((iter_a != a.cend()) || (iter_b != b.cend())) {
+    return false;
+  }
+  return true;
+}
 
 }  // namespace util
 }  // namespace expr
-
-#include "expr/expr_util-inl.h"
 
 #endif  // EXPR_EXPR_UTIL_H_
