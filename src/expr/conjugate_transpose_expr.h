a23e3531f8725427391469230302dcdd1d491481
Michael Nett <mnett@google.com>
2015-01-06 11:46:22 +0900

    Unifying conjugate transpose and transpose. Expanding unit tests.

--- src/expr/conjugate_transpose_expr.h
+++ src/expr/conjugate_transpose_expr.h
@@ -2,51 +2,48 @@
 #define EXPR_CONJUGATE_TRANSPOSE_EXPR_H_
 
 #include <complex>
+#include <type_traits>
+
 #include "expr/matrix_expr.h"
 #include "tensor/storage_mode.h"
 
 namespace expr {
+namespace {
 
-namespace internal {
-  template <class T>
-  typename std::enable_if<std::is_same<std::complex<float>, T>::value ||
-      std::is_same<std::complex<double>, T>::value ||
-      std::is_same<std::complex<long double>, T>::value, T>::type conjugate(T t) {
-    return std::conj(t);
-  }
+template <typename T>
+typename std::enable_if<std::is_same<std::complex<float>, T>::value ||
+                            std::is_same<std::complex<double>, T>::value ||
+                            std::is_same<std::complex<long double>, T>::value,
+                        T>::type
+Conjugate(const T& value) {
+  return std::conj(value);
+}
 
-  template <typename T>
-  typename std::enable_if<std::is_floating_point<T>::value, T>::type conjugate(T t) {
-    return t;
-  }
+template <typename T>
+typename std::enable_if<std::is_floating_point<T>::value, T>::type Conjugate(
+    const T& value) {
+  return value;
+}
 
-  template <bool Conj, typename ValueType> struct Conjugator {
-    static const ValueType apply(const ValueType& value);
-  };
+}  // namespace
 
-  template <typename ValueType> struct Conjugator<true, ValueType> {
-    static const ValueType apply(const ValueType& value) { return conjugate(value); }
-  };
-
-  template <typename ValueType> struct Conjugator<false, ValueType> {
-    static const ValueType apply(const ValueType& value) { return value; }
-  };
-} // namespace internal
-
-template <typename ExprType, typename ValueType, typename Conj = internal::Conjugator<true, ValueType>>
+template <typename ExprType, typename ValueType>
 class ConjugateTransposeExpr
-    : public MatrixExpr<ConjugateTransposeExpr<ExprType, ValueType, Conj>, ValueType> {
+    : public MatrixExpr<ConjugateTransposeExpr<ExprType, ValueType>,
+                        ValueType> {
  public:
-  using base_type  = MatrixExpr<ConjugateTransposeExpr<ExprType, ValueType, Conj>, ValueType>;
-  using size_type  = typename base_type::size_type;
-  using pos_type   = typename base_type::pos_type;
+  using base_type =
+      MatrixExpr<ConjugateTransposeExpr<ExprType, ValueType>, ValueType>;
+  using size_type = typename base_type::size_type;
+  using pos_type = typename base_type::pos_type;
   using value_type = typename base_type::value_type;
 
-  // Create a scalar product expression representing the product of a scalar and a tensor.
+  // Create a scalar product expression representing the product of a scalar and
+  // a tensor.
   ConjugateTransposeExpr(const ExprType& expr) : expr_(expr) {}
 
   pos_type dimensions() const override {
-    return expr_.dimensions();
+    return pos_type { expr_.dimensions()[1], expr_.dimensions()[0] };
   }
 
   size_type size() const override {
@@ -54,7 +51,7 @@ class ConjugateTransposeExpr
   }
 
   const value_type Get(const pos_type& index) const override {
-    return Conj::apply(expr_.Get(pos_type { index[1], index[0] }));
+    return Conjugate<value_type>(expr_.Get(pos_type{index[1], index[0]}));
   }
 
   value_type abs() const override {
--- src/test/conjugate_transpose_expr_test.cc
+++ src/test/conjugate_transpose_expr_test.cc
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <complex>
 
 #include "expr/conjugate_transpose_expr.h"
 #include "tensor/storage_mode.h"
@@ -11,38 +12,90 @@ using tensor::Matrix;
 namespace test {
 namespace {
 
-Matrix<float> CreateMatrix() {
-  Matrix<float> tensor({{2, 2}}, StorageMode::DENSE);
-  tensor.Set({{0, 0}}, 1.0f);
-  tensor.Set({{0, 1}}, 2.0f);
-  tensor.Set({{1, 0}}, 3.0f);
-  tensor.Set({{1, 1}}, 4.0f);
-  return tensor;
+Matrix<float> CreateRealMatrix() {
+  Matrix<float> matrix({{3, 2}}, StorageMode::DENSE);
+  matrix.Set({{0, 0}}, 1.0f);
+  matrix.Set({{0, 1}}, 2.0f);
+  matrix.Set({{1, 0}}, 3.0f);
+  matrix.Set({{1, 1}}, 4.0f);
+  matrix.Set({{2, 0}}, 7.0f);
+  matrix.Set({{2, 1}}, 5.0f);
+  return matrix;
+}
+
+Matrix<std::complex<float>> CreateComplexMatrix() {
+  Matrix<std::complex<float>> matrix({{2, 3}}, StorageMode::DENSE);
+  matrix.Set({{0, 0}}, std::complex<float>(1.0f, 1.0f));
+  matrix.Set({{0, 1}}, std::complex<float>(5.0f, 2.0f));
+  matrix.Set({{0, 2}}, std::complex<float>(3.0f, -1.0f));
+  matrix.Set({{1, 0}}, std::complex<float>(1.0f, -3.0f));
+  matrix.Set({{1, 1}}, std::complex<float>(-2.0f, 4.0f));
+  matrix.Set({{1, 2}}, std::complex<float>(-1.0f, 2.0f));
+  return matrix;
 }
 
 }  // namespace
 
-TEST(ConjugateTransposeExprTest, SanityCheck) {
-  // Create a tensor and verify its norm.
-  Matrix<float> tensor = CreateMatrix();
-  const auto conj = ~tensor;
-  // EXPECT_FLOAT_EQ(5.477225575, tensor.abs());
+TEST(ConjugateTransposeExprTest, ShouldConjugateTransposeComplexMatrix) {
+  auto matrix = CreateComplexMatrix();
+  auto transpose = ~matrix;
 
-  // // Scaling a tensor by 1 or -1 must preserve its norm.
-  // {
-  //   auto pos_scaled = 1.0f * tensor;
-  //   EXPECT_FLOAT_EQ(5.477225575, pos_scaled.abs());
-  //   auto neg_scaled = -1.0f * tensor;
-  //   EXPECT_FLOAT_EQ(5.477225575, neg_scaled.abs());
-  // }
+  ASSERT_EQ(2, transpose.dimensions().size());
+  EXPECT_EQ(3, transpose.dimensions()[0]);
+  EXPECT_EQ(2, transpose.dimensions()[1]);
 
-  // // Tensor scaling should be positively homogenous.
-  // {
-  //   auto pos_scaled = 2.0f * tensor;
-  //   EXPECT_FLOAT_EQ(2.0f * 5.477225575f, pos_scaled.abs());
-  //   auto neg_scaled = -2.0f * tensor;
-  //   EXPECT_FLOAT_EQ(2.0f * 5.477225575f, neg_scaled.abs());
-  // }
+  EXPECT_EQ(std::complex<float>(1.0f, -1.0f), transpose.Get({{0, 0}}));
+  EXPECT_EQ(std::complex<float>(-1.0f, -2.0f), transpose.Get({{2, 1}}));
+}
+
+TEST(ConjugateTransposeExprTest, ConjugateTransposeShouldBeSelfInverse) {
+  auto matrix = CreateComplexMatrix();
+  auto same_matrix = ~~matrix;
+
+  // TODO(mnett): Integrate google-mock to use container matchers.
+  // TODO(mnett): Test against fixed values.
+  ASSERT_EQ(2, matrix.dimensions().size());
+  ASSERT_EQ(2, same_matrix.dimensions().size());
+  EXPECT_EQ(matrix.dimensions()[0], same_matrix.dimensions()[0]);
+  EXPECT_EQ(matrix.dimensions()[1], same_matrix.dimensions()[1]);
+
+  EXPECT_EQ(matrix.Get({{0, 0}}), same_matrix.Get({{0, 0}}));
+  EXPECT_EQ(matrix.Get({{0, 1}}), same_matrix.Get({{0, 1}}));
+  EXPECT_EQ(matrix.Get({{0, 2}}), same_matrix.Get({{0, 2}}));
+  EXPECT_EQ(matrix.Get({{1, 0}}), same_matrix.Get({{1, 0}}));
+  EXPECT_EQ(matrix.Get({{1, 1}}), same_matrix.Get({{1, 1}}));
+  EXPECT_EQ(matrix.Get({{1, 2}}), same_matrix.Get({{1, 2}}));
+}
+
+TEST(ConjugateTransposeExprTest, ShouldTransposeRealMatrix) {
+  auto matrix = CreateRealMatrix();
+  auto transpose = ~matrix;
+
+  ASSERT_EQ(2, transpose.dimensions().size());
+  EXPECT_EQ(2, transpose.dimensions()[0]);
+  EXPECT_EQ(3, transpose.dimensions()[1]);
+
+  EXPECT_EQ(1.0f, transpose.Get({{0, 0}}));
+  EXPECT_EQ(5.0f, transpose.Get({{1, 2}}));
+}
+
+TEST(ConjugateTransposeExprTest, TransposeShouldBeSelfInverse) {
+  auto matrix = CreateRealMatrix();
+  auto same_matrix = ~~matrix;
+
+  // TODO(mnett): Integrate google-mock to use container matchers.
+  // TODO(mnett): Test against fixed values.
+  ASSERT_EQ(2, matrix.dimensions().size());
+  ASSERT_EQ(2, same_matrix.dimensions().size());
+  EXPECT_EQ(matrix.dimensions()[0], same_matrix.dimensions()[0]);
+  EXPECT_EQ(matrix.dimensions()[1], same_matrix.dimensions()[1]);
+
+  EXPECT_EQ(matrix.Get({{0, 0}}), same_matrix.Get({{0, 0}}));
+  EXPECT_EQ(matrix.Get({{0, 1}}), same_matrix.Get({{0, 1}}));
+  EXPECT_EQ(matrix.Get({{1, 0}}), same_matrix.Get({{1, 0}}));
+  EXPECT_EQ(matrix.Get({{1, 1}}), same_matrix.Get({{1, 1}}));
+  EXPECT_EQ(matrix.Get({{2, 0}}), same_matrix.Get({{2, 0}}));
+  EXPECT_EQ(matrix.Get({{2, 1}}), same_matrix.Get({{2, 1}}));
 }
 
 }  // namespace test
